c3eca8ab44c17680581b2c50a517ed220c8c20e7
Randy <31799713+randy-r@users.noreply.github.com>
2018-04-28 11:38:01 +0300

    New tick sound and tick interval.

--- src/components/Questionnaire/ProgressTimer.jsx
+++ src/components/Questionnaire/ProgressTimer.jsx
@@ -2,35 +2,47 @@ import React, { Component, Fragment } from 'react';
 import { Progress } from 'antd';
 
 const tickingSound = new Audio(`${process.env.PUBLIC_URL}/Ticking-clock-sound.mp3`);
-tickingSound.playbackRate = 2;
+tickingSound.playbackRate = 1;
 tickingSound.loop = true;
 
 class ProgressTimer extends Component {
   state = { percent: 100 }
+  substractionInterval = 500
+
+  computeNewPercent = () => {
+    const { totalTime } = this.props;
+    const percentToSubstract = 100 / (totalTime / this.substractionInterval);
+    const { percent } = this.state;
+    let newPercent = percent - percentToSubstract;
+    newPercent = newPercent < 0 ? 0 : newPercent;
+    return newPercent;
+  }
 
   componentDidMount() {
-    const { totalTime, onTimeRanOut } = this.props;
-    const percentToSubstract = 100 / (totalTime / 1000);
+    const { onTimeRanOut } = this.props;
+
+    let newPercent;
+    newPercent = this.computeNewPercent();
+    this.setState({ percent: newPercent });
 
     tickingSound.play();
-    this.timeoutId = setTimeout(
+    this.intervalId = setInterval(
       () => {
-        const { percent } = this.state;
-        let newPercent = percent - percentToSubstract;
-        newPercent = newPercent < 0 ? 0 : newPercent;
-        if (newPercent <= 0) {
-          tickingSound.pause();
-          clearTimeout(this.timeoutId);
-          onTimeRanOut();
-        }
-        this.setState({ percent: newPercent })
-      }, 1000
+        newPercent = this.computeNewPercent();
+        this.setState({ percent: newPercent }, () => {
+          if (newPercent <= 0) {
+            tickingSound.pause();
+            clearTimeout(this.intervalId);
+            onTimeRanOut();
+          }
+        });
+      }, this.substractionInterval
     );
   }
 
   componentWillUnmount() {
     tickingSound.pause();
-    clearTimeout(this.timeoutId);    
+    clearTimeout(this.intervalId);
   }
 
   render() {
